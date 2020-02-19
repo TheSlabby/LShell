359d1430043ee17f1678a2d533ce5362ade450b3
Walker McGilvary <walker.mcgilvary@gmail.com>
2020-02-19 13:00:22 -0600

    fixed build

--- src/com/github/theslabby/lshell/ServerMain.java
+++ src/com/github/theslabby/lshell/ServerMain.java
@@ -6,11 +6,18 @@ import com.github.theslabby.lshell.common.ServerSocket;
 public class ServerMain {
 
 	public static final String OS = System.getProperty("os.name").toLowerCase();
+	public static int port = 4545;
 	
 	public static void main(String[] args) {
 		
+		  try {
+		        port = Integer.parseInt(args[0]);
+		    } catch (Exception e) {
+		    	System.out.println("Didn't find port, using 4545");
+		    }
+		
 		//start listening
-		ServerSocket serverThread = new ServerSocket(4545);
+		ServerSocket serverThread = new ServerSocket(port);
 		serverThread.start();
 		
 		//start reading for console commands
